ffe5fc5b78db620e4aecce3982ebcdc98c4099d0
Sevenblade11 <nicjen99@gmail.com>
2022-05-12 09:27:23 -0500

    Tooltip has been implemented, as well as tool tip information for power ups.

--- Assets/Scripts/ToolTip.cs
+++ Assets/Scripts/ToolTip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,34 +8,23 @@ using UnityEngine.UIElements;
 
 public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    GameObject frame;
-    Text infoText;
-    Vector3 min, max;
-    Camera mainCamera;
-    RectTransform rect;
-    float offset = 10f;
-    
+    private GameObject frame;
+    public Text infoText;
+    public String info;
 
     void Start(){
         frame = GameObject.FindGameObjectWithTag("Tooltip");
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        //infoText.text = gameObject.GetComponent<PowerUp>().information.text;
-        rect = frame.GetComponent<RectTransform>();
-  
-        min = new Vector3(0,0,0);
-        max = new Vector3(mainCamera.pixelWidth, mainCamera.pixelHeight, 0);
+        infoText = GameObject.FindGameObjectWithTag("TooltipInfo").GetComponent<Text>();
+
         if(frame != null)
-            frame.transform.position = new Vector3(-1000,-1000,-1000);
+            frame.transform.position = new Vector3(-365,0,-20);
     }
 
     public void OnPointerEnter(PointerEventData data){
-        frame.transform.position = new Vector3(0,0,0);
-
-        //Vector3 position = new Vector3(Input.mousePosition.x + rect.rect.width, 
-        //Input.mousePosition.y - (rect.rect.height / 2 + offset), 0f);
-        
-        //frame.transform.position = new Vector3(Mathf.Clamp(position.x, min.x + rect.rect.width/2, max.x - rect.rect.width/2),
-        //Mathf.Clamp(position.y, min.y + rect.rect.height / 2, max.y - rect.rect.height / 2), position.z);
+        frame.SetActive(false);
+        infoText.text = info;
+        frame.transform.position = new Vector3(620,70,0);
+        frame.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData data){
